ff8715c9e282b9caf7eea393bd2b3583500274a5
Nicole Monteiro Torres <duq0104759@aluno.firjansenaisesi.com.br>
2026-03-13 16:02:16 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+ 
+const rotas = require("./controller/controller");
+ 
+app.use("/", rotas);
+ 
+const PORT = 3055;
+ 
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
