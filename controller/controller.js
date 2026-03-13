5f12ad50aaf5b9ce8edddf34bec1e87d05bd5b3f
Nicole Monteiro Torres <duq0104759@aluno.firjansenaisesi.com.br>
2026-03-13 16:19:15 -0300

    Segundo commit

--- controller/controller.js
+++ controller/controller.js
@@ -1,13 +1,41 @@
 const express = require("express");
-const app = express();
- 
-const rotas = require("./controller/controller");
- 
-app.use("/", rotas);
- 
-const PORT = 3055;
- 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+const router = express.Router();
+
+const fs = require("fs");
+const path = require("path");
+
+// caminho do bd.json
+const caminho = path.join(__dirname, "../repository/bd.json");
+
+
+// ROTA /usuario
+router.get("/usuario", (req, res) => {
+
+    const dados = fs.readFileSync(caminho);
+    const usuarios = JSON.parse(dados);
+
+    res.json(usuarios);
+
 });
- 
+
+
+// ROTA /site
+router.get("/site", (req, res) => {
+
+    res.send(`Bem-vindo ao servidor Node.js
+Este é um exemplo de rota simples.`);
+
+});
+
+
+// ROTA /calculo/:valor
+router.get("/calculo/:valor", (req, res) => {
+
+    const valor = req.params.valor;
+    const resultado = valor * valor;
+
+    res.send(`O valor ${valor} elevado ao quadrado é ${resultado}`);
+
+});
+
+module.exports = router;
